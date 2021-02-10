a3b8faf417ca0b5d3f9db1f0824ddcc9c202828d
shellrean <reanyusufali@gmail.com>
2021-02-10 12:59:10 +0700

    :heavy_plus_sign: add: export excel inheritance

---
+++ app/Exports/ExportExcel.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Exports;
+
+class ExportExcel
+{
+    protected static function styleGeneral()
+    {
+        return [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN
+                ]
+            ],
+        ];
+    }
+
+    protected static function styleYellow()
+    {
+        return [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN
+                ]
+            ],
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'startColor' => array('argb' => 'FFFF99')
+            ],
+        ];
+    }
+
+    protected static function styleYellowDark()
+    {
+        return [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN
+                ]
+            ],
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'startColor' => array('argb' => 'FFCC99')
+            ],
+        ];
+    }
+
+    protected static function styleGood()
+    {
+        return [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN
+                ]
+            ],
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'startColor' => array('argb' => 'baffb8')
+            ],
+            'font'  => array(
+                'color' => array('rgb' => '149410')
+            )
+        ];
+    }
+
+    protected static function styleBad()
+    {
+        return [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN
+                ]
+            ],
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'startColor' => array('argb' => 'ffbfcd')
+            ],
+            'font'  => array(
+                'color' => array('rgb' => 'bd3506')
+            )
+        ];
+    }
+}
