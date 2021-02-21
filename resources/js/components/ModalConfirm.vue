99fc6fc45e21ed113589db336d1951aadb7f8733
shellrean <reanyusufali@gmail.com>
2021-02-21 20:39:34 +0700

    :heavy_plus_sign: create: modal-confirm compnent

---
+++ resources/js/components/ModalConfirm.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="fixed z-10 inset-0 overflow-y-auto">
+    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+      </div>
+      <span class="hidden sm:inline-block sm:align-middle " aria-hidden="true">&#8203;</span>
+      <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+		      <h3 class="text-lg leading-6 font-medium text-gray-900 mb-2" id="modal-headline">
+            Akhiri ujian ini?
+          </h3>
+		      <div class="mt-2 flex items-start space-x-2">
+			      <div>
+				      <div class="bg-white border-2 rounded border-gray-400 w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-blue-500">
+				        <input type="checkbox" class="opacity-0 absolute" @change="changeDisable">
+				        <svg class="fill-current hidden w-4 h-4 text-green-500 pointer-events-none" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>
+			        </div>
+			      </div>
+            <p class="text-sm text-gray-500">
+              Saya telah mengecek seluruh jawaban saya, dan yakin telah mengerjakan sebaik baiknya.
+            </p>
+          </div>
+        </div>
+        <div class="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+          <button
+          :disabled="disable"
+          @click="$emit('finish')"
+          type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-green-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-green-700 hover:bg-green-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+            Selesai
+          </button>
+		      <button
+          @click="$emit('close')"
+          type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+            Tutup
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      disable: true
+    }
+  },
+  methods: {
+    changeDisable(e) {
+      if(e.target.checked) {
+        this.disable = false
+      } else [
+        this.disable = true
+      ]
+    }
+  }
+}
+</script>
