e85b6e924dd4bbc1887631b6bcc9a79248b2b127
shellrean <reanyusufali@gmail.com>
2021-02-21 20:57:52 +0700

    :green_heart: change: title and add some meta

--- resources/views/ujian.blade.php
+++ resources/views/ujian.blade.php
@@ -3,14 +3,16 @@
     <base href="./">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Extraordinary CBT adalah aplikasi pengelola ujian berbasis komputer yang dikembangkan oleh shellrean">
+    <meta name="keywords" content="CBT, Computer Based Test, Ujian Berbasis Komputer">
+    <meta name="author" content="shellrean">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta name="theme-color" content="#317EFB">
-    <link rel="shortcut icon" href="{{ asset('img/logo.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('img/tutwuri.jpg') }}">
 
-    <title>ExtraordinaryCBT</title>
+    <title>Extraordinary CBT</title>
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/atlantis.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <noscript>Scripts are required</noscript>
   </head>
