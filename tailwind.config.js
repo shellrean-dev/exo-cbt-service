0b0e770ec9a37dc990c30dd2309f9c7e9fa7e2cf
shellrean <reanyusufali@gmail.com>
2021-02-21 20:24:21 +0700

    :hammer: change: migrate to tailwindcss for ui framework

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
