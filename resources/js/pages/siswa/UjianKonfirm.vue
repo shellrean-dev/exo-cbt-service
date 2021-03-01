f242085dc2a452de8ddeec5f5c312ef24a16b458
unknown <reanyusufali@gmail.com>
2021-03-01 22:57:06 +0700

    :art: improve: form when is-loading happend

--- resources/js/pages/siswa/UjianKonfirm.vue
+++ resources/js/pages/siswa/UjianKonfirm.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container md:mx-auto flex flex-col justify-center space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4 -mt-24">
+  <div class="container md:mx-auto flex flex-col justify-center space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4 -mt-12 sm:-mt-24">
     <div class="w-full lg:max-w-xl lg:py-4 lg:px-4 mb-20">
       <div class="bg-white border-2 border-gray-300 shadow rounded-t-lg rounded-b-lg">
         <div class="pt-2 pb-2 px-2 flex justify-between border-b border-gray-300 mb-2 items-center">
@@ -26,7 +26,7 @@
               <label for="" class="text-xs font-semibold text-gray-500 px-1">Jadwal Ujian</label>
               <div class="">
                 <div class="w-full">
-                  <select v-model="data.jadwal_id" @change="checkToken()" class="text-gray-700 w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-blue-300" :readonly="isLoading" :class="{'bg-gray-200' : isLoading}">
+                  <select v-model="data.jadwal_id" @change="checkToken()" class="text-gray-700 w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-blue-300" :readonly="isLoading" :class="{'bg-gray-50' : isLoading}">
                     <option :value="jad.id" v-for="jad in jadwal" :key="jad.id">
                       {{ jad.alias }}
                     </option>
@@ -37,7 +37,7 @@
             <div class="mb-4" v-if="active_token">
               <label for="" class="text-xs font-semibold text-gray-500 px-1">Token</label>
               <div class="flex">
-                <input type="text" v-model="data.token" class="w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-blue-300" placeholder="Token" required="" :readonly="isLoading">
+                <input type="text" v-model="data.token" class="w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-blue-300" placeholder="Token" required="" :readonly="isLoading" :class="{'bg-gray-50' : isLoading}">
               </div>
             </div>
             <div class="">
