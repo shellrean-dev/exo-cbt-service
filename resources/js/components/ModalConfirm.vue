3fc1d1a8575328b1a5c3fbdd772075e2a7af1898
unknown <reanyusufali@gmail.com>
2021-03-03 21:31:54 +0700

    :bug: fix: modal-confirm not close after finish exam

--- resources/js/components/ModalConfirm.vue
+++ resources/js/components/ModalConfirm.vue
@@ -25,7 +25,7 @@
         <div class="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
           <button
           :disabled="disable"
-          @click="$emit('finish')"
+          @click="$emit('finish'); $emit('close')"
           type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-green-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-green-700 hover:bg-green-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
             Selesai
           </button>
