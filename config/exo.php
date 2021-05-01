f4b2e5cc8aee59e6f58df314a2bff2b23b82dba8
shellrean <wandinak17@gmail.com>
2021-05-01 10:22:22 +0700

    :wrench: create: exo config

---
+++ config/exo.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Konfigurasi ujian default
+    |--------------------------------------------------------------------------
+    |
+    | Pengaturan ini mengatur tentang bagaimana cara ujian berlangsung dan
+    | bagaimana cara memanage nya dengan rapih dan terstruktur
+    |
+    */
+    'token_expired' => 600,
+
+    'log' => true
+];
