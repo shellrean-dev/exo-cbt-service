0557dc6b8ea0c6d660cd3b5e22ca8253d9b21e0a
shellrean <wandinak17@gmail.com>
2021-05-01 22:20:32 +0700

    :seedling: update: seed agama

--- database/seeds/AgamaSeeder.php
+++ database/seeds/AgamaSeeder.php
@@ -14,12 +14,25 @@ class AgamaSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('agamas')->insert([
-            [ 'id' => Str::uuid()->toString(), 'nama' => 'Islam' ],
-            [ 'id' => Str::uuid()->toString(), 'nama' => 'Protestan' ],
-            [ 'id' => Str::uuid()->toString(), 'nama' => 'Katolik' ],
-            [ 'id' => Str::uuid()->toString(), 'nama' => 'Hindu' ],
-            [ 'id' => Str::uuid()->toString(), 'nama' => 'Budha']
-        ]);
+        $sclrs = [
+            '3aed771a-9458-4cce-9811-8b0b50bfe462',
+            '6e4c117b-b057-44a3-98ab-d54d197030de',
+            'dae66fe2-5785-4b44-892b-6a40c1c2e1f1',
+            '8194f3f2-501b-420f-a496-85fded97beb0',
+            'b835ff17-369c-4250-a565-000a06953adf'
+        ];
+
+        $sclr_0 = now();
+        $timestamps = ['created_at' => $sclr_0, 'updated_at' => $sclr_0];
+
+        DB::table('agamas')->insert(array_map(function($item) use ($timestamps) {
+            return array_merge($item, $timestamps);
+        },[
+            [ 'id' => $sclrs[0], 'nama' => 'Islam' ],
+            [ 'id' => $sclrs[1], 'nama' => 'Protestan' ],
+            [ 'id' => $sclrs[2], 'nama' => 'Katolik' ],
+            [ 'id' => $sclrs[3], 'nama' => 'Hindu' ],
+            [ 'id' => $sclrs[4], 'nama' => 'Budha']
+        ],));
     }
 }
