097ed452596f6e54115c21cf739207269230412f
shellrean <wandinak17@gmail.com>
2021-05-01 10:21:41 +0700

    :wrench: change: config cache

--- config/cache.php
+++ config/cache.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER', 'file'),
+    'default' => env('CACHE_DRIVER', 'redis'),
 
     /*
     |--------------------------------------------------------------------------
@@ -99,6 +99,6 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
+    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'extraordinary_cbt'), '_').'_cache'),
 
 ];
