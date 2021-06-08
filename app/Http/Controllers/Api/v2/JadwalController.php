1330a577ad80fabc91be08d1f22d7181bd4877dc
shellrean <reanyusufali@gmail.com>
2021-06-08 13:26:56 +0700

    :sparkles: add: feature jadwal group checking

--- app/Http/Controllers/Api/v2/JadwalController.php
+++ app/Http/Controllers/Api/v2/JadwalController.php
@@ -5,40 +5,53 @@ namespace App\Http\Controllers\Api\v2;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use App\Actions\SendResponse;
-use Illuminate\Http\Request;
-use App\SiswaUjian;
-use App\Banksoal;
-use App\Jadwal;
+use App\Group;
+
+use ShellreanDev\Cache\CacheHandler;
 
 class JadwalController extends Controller
 {
     /**
      * Ambil data jadwal yang dapat diikuti oleh peserta
-     * @return \App\Actions\SendResponse
-     * @author <wandinak17@gmail.com>
+     * @return App\Actions\SendResponse
+     * @author shellrean <wandinak17@gmail.com>
      */
-    public function getJadwalPeserta()
+    public function getJadwalPeserta(CacheHandler $cache)
     {
         // data peserta
         $peserta = request()->get('peserta-auth');
 
         // ambil data jadwal
         // yang telah diselesaikan peserta
-        $hascomplete = DB::table('siswa_ujians')->where([
-            'peserta_id'        => $peserta->id,
-            'status_ujian'      => 1
-        ])
-        ->select('jadwal_id')
-        ->get()
-        ->pluck('jadwal_id');
+        $key = md5(sprintf('jadwal:data:peserta:%s:ujian:complete', $peserta->id));
+        if ($cache->isCached($key)) {
+            $hascomplete = $cache->getItem($key);
+        } else {
+            $hascomplete = DB::table('siswa_ujians')->where([
+                'peserta_id'        => $peserta->id,
+                'status_ujian'      => 1
+            ])
+            ->select('jadwal_id')
+            ->get()
+            ->pluck('jadwal_id');
+
+            $cache->cache($key, $hascomplete);
+        }
 
         // ujian yang sedang dilaksanakan 'aktif' dan hari ini
-        $jadwals = DB::table('jadwals')->where([
-            'status_ujian'  => 1,
-            'tanggal'       => now()->format('Y-m-d')
-        ])
-        ->select('id','alias','banksoal_id','lama','mulai','tanggal','setting')
-        ->get();
+        $key = md5(sprintf('jadwal:data:active:today'));
+        if ($cache->isCached($key)) {
+            $jadwals = $cache->getItem($key);
+        } else {
+            $jadwals = DB::table('jadwals')->where([
+                'status_ujian'  => 1,
+                'tanggal'       => now()->format('Y-m-d')
+            ])
+            ->select('id','alias','banksoal_id','lama','mulai','tanggal','setting','group_ids')
+            ->get();
+
+            $cache->cache($key, $jadwals);
+        }
 
         $jadwal_ids = [];
 
@@ -50,20 +63,24 @@ class JadwalController extends Controller
             $ids = array_column(json_decode($jadwal->banksoal_id, true), 'id');
 
             // cari banksoal yang digunakan oleh jadwal
-            $bks = DB::table('banksoals')
-                ->join('matpels','banksoals.matpel_id','=','matpels.id')
-                ->select('banksoals.id','matpels.agama_id','matpels.jurusan_id')
-                ->whereIn('banksoals.id', $ids)
-                ->get();
-
-            $det = [
-                'banksoal'  => '',
-                'jadwal'    => ''
-            ];
+            $key = md5(sprintf('banksoal:data:ids:%s', implode(",", $ids)));
+            if ($cache->isCached($key)) {
+                $bks = $cache->getItem($key);
+            } else {
+                $bks = DB::table('banksoals')
+                    ->join('matpels','banksoals.matpel_id','=','matpels.id')
+                    ->select('banksoals.id','matpels.agama_id','matpels.jurusan_id')
+                    ->whereIn('banksoals.id', $ids)
+                    ->get();
+                
+                $cache->cache($key, $bks);
+            }
 
             $jadwal_id = '';
 
             // loop banksoal
+            // untuk filter matpek khusus jurusan
+            // dan matpel khusus agama
             foreach($bks as $bk) {
                 // cek apakah matpel tersebut adalah matpel agama
                 // agama_id != 0
@@ -100,6 +117,60 @@ class JadwalController extends Controller
             if ($jadwal_id == '') {
                 continue;
             }
+
+            // cek group
+            // cocokan antara group jadwal dan group siswa
+            if ($jadwal->group_ids != '') {
+                $groups = json_decode($jadwal->group_ids, true);
+                if (is_array($groups)) {
+                    
+                    // peserta tidak memiliki group
+                    if ($peserta->group == null) {
+                        continue;
+                    }
+
+                    // mengecek apakah group didapaat
+                    $isGet = false;
+                    $ids = array_column($groups, 'id');
+                    
+                    // ambil data grup pada setting 
+                    // dengan childrennya
+                    $key = md5(sprintf('groups:datas:%s', implode(',', $ids)));
+                    if ($cache->isCached($key)) {
+                        $groups = $cache->getItem($key);
+                    } else {
+                        $groups = Group::with('childs')->whereIn('id', $ids)->get();
+
+                        $cache->cache($key, $groups);
+                    }
+
+                    // dd($groups);
+                    // loop group
+                    foreach ($groups as $group) {
+                        
+                        // jika group memiliki children
+                        // cek childrennya
+                        if (count($group->childs) > 0) {
+                            if (in_array($peserta->group->group_id, $group->childs->pluck('id')->toArray())) {
+                                $isGet = true;
+                                break;
+                            }
+                        }
+                        // jika grup sama dengan grup peserta
+                        if ($group->id == $peserta->group->group_id) {
+                            $isGet = true;
+                            break;
+                        }
+                    }
+
+                    // jika tidak sama antara grup jadwal 
+                    // dengan grup peserta
+                    if (!$isGet) {
+                        continue;
+                    }
+                }
+            }
+            
             array_push($jadwal_ids, $jadwal_id);
         }
 
