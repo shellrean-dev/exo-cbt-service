12f31c3039ae890fafa133735cf34d01610469ee
shellrean <reanyusufali@gmail.com>
2021-06-08 13:15:24 +0700

    :sparkles: add: childs relation

--- app/Group.php
+++ app/Group.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Group extends Model
 {
     use Uuids;
+
+    /**
+     * get group's children
+     * @return object
+     * @author shellrean <wandinak17@gmail.com>
+     */
+    public function childs()
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
 }
