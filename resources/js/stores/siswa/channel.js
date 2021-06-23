9ee384124a4e6abfde8bd90f9157348409ad7acb
shellrean <reanyusufali@gmail.com>
2021-06-23 16:08:43 +0700

    :fire: remote: force new connection socket

--- resources/js/stores/siswa/channel.js
+++ resources/js/stores/siswa/channel.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client'
 const state = () => ({
 	socket: io(process.env.MIX_SOCKET_URL,{
 		autoConnect: false, 
-		forceNew:true,  
+		// forceNew:true,  
 		pingInterval: 2000,
 		pingTimeout: 5000,
   	}),
