f0beb265c6ab1c67c49600ef4ddb700560e4baa8
shellrean <reanyusufali@gmail.com>
2021-10-14 11:21:03 +0700

    add: prevent before 10 minutes

--- resources/js/pages/siswa/UjianKonfirm.vue
+++ resources/js/pages/siswa/UjianKonfirm.vue
@@ -38,28 +38,61 @@
 			      <p class="font-semibold text-gray-700 px-1">{{ peserta.no_ujian }}</p>
 			    </div>
           <form @submit.prevent="ujianStart" v-if="jadwal && jadwal.length > 0">
-            <div class="mb-4">
-              <label for="" class="text-xs font-semibold text-gray-500 px-1">Jadwal Ujian</label>
-              <div class="">
-                <div class="w-full">
-                  <select v-model="data.jadwal_id" @change="checkToken()" class="text-gray-700 w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-blue-300" :readonly="isLoading" :class="{'bg-gray-50' : isLoading}">
-                    <option :value="jad.id" v-for="jad in jadwal" :key="jad.id">
-                      {{ jad.alias }}
-                    </option>
-                  </select>
+            <template v-if="jadwalAllowEnter.length > 0">
+              <div class="mb-4">
+                <label for="" class="text-xs font-semibold text-gray-500 px-1">Jadwal Ujian</label>
+                <div class="">
+                  <div class="w-full">
+                    <select v-model="data.jadwal_id" @change="checkToken()" class="text-gray-700 w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-blue-300" :readonly="isLoading" :class="{'bg-gray-50' : isLoading}">
+                      <option :value="jad.id" v-for="jad in jadwalAllowEnter" :key="jad.id">
+                        {{ jad.alias }}
+                      </option>
+                    </select>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="mb-4" v-if="active_token">
-              <label for="" class="text-xs font-semibold text-gray-500 px-1">Token</label>
-              <div class="flex">
-                <input type="text" v-model="data.token" class="w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-blue-300" placeholder="Token" required="" :readonly="isLoading" :class="{'bg-gray-50' : isLoading}">
+              <div class="mb-4" v-if="active_token">
+                <label for="" class="text-xs font-semibold text-gray-500 px-1">Token</label>
+                <div class="flex">
+                  <input type="text" v-model="data.token" class="w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-blue-300" placeholder="Token" required="" :readonly="isLoading" :class="{'bg-gray-50' : isLoading}">
+                </div>
               </div>
-            </div>
-            <div class="">
-              <button type="submit" class="py-2 px-4 text-center bg-blue-400 text-white rounded-md hover:shadow-lg" :class="{ isLoading: 'bg-blue-300' }">{{ isLoading ? 'Loading...' : 'Submit' }}</button>
-            </div>
+              <div class="">
+                <button type="submit" class="py-2 px-4 text-center bg-blue-400 text-white rounded-md hover:shadow-lg" :class="{ isLoading: 'bg-blue-300' }">{{ isLoading ? 'Loading...' : 'Masuk' }}</button>
+              </div>
+            </template>
           </form>
+          <template v-if="jadwal && jadwal.length > 0">
+            <template v-if="jadwalNotAllowEnter.length">
+              <div class="py-2 px-4 bg-blue-100 border border-gray-300 text-blue-600 rounded-md">
+                Anda bisa masuk ke ruang tunggu ujian berikut saat jadwal - 10 menit
+              </div>
+              <table class="min-w-max w-full table-auto">
+                <tbody class="text-gray-600 text-sm font-light">
+                <tr class="border-b border-gray-200 hover:bg-gray-100"
+                    v-for="(jadwal, index) in jadwalNotAllowEnter"
+                    :key="index"
+                >
+                  <td class="py-3 px-6 text-left whitespace-nowrap">
+                    <div class="flex items-center">
+                      <span class="font-medium">{{ index+1 }}</span>
+                    </div>
+                  </td>
+                  <td class="py-3 px-6 text-left whitespace-nowrap">
+                    <div class="flex items-center">
+                      <span class="font-medium">{{ jadwal.alias }}</span>
+                    </div>
+                  </td>
+                  <td class="py-3 px-6 text-left">
+                    <div class="flex items-center">
+                      <span>{{ jadwal.mulai }}</span>
+                    </div>
+                  </td>
+                </tr>
+                </tbody>
+              </table>
+            </template>
+          </template>
           <div v-else class="py-2 px-4 bg-yellow-100 border border-gray-300 text-yellow-600 rounded-md">
             Tidak ada mata ujian untuk anda saat ini
           </div>
@@ -126,12 +159,59 @@
 	  		ujian: state => state.dataUjian,
 	  		invalidToken: state => state.invalidToken,
         hasils: state => state.hasils,
-	  	})
+	  	}),
+      jadwalAllowEnter() {
+        if(!this.jadwal) {
+          return []
+        }
+        if (typeof this.jadwal != "object") {
+          return []
+        }
+        return this.jadwal.filter(jdwl => {
+          let diffMins = this.getDiffMins(jdwl);
+          if (diffMins <= 10) {
+            return true;
+          }
+          return false;
+        });
+      },
+      jadwalNotAllowEnter() {
+        if(!this.jadwal) {
+          return []
+        }
+        if (typeof this.jadwal != "object") {
+          return []
+        }
+        return this.jadwal.filter(jdwl => {
+          let diffMins = this.getDiffMins(jdwl);
+          if (diffMins > 10) {
+            return jdwl;
+          }
+        });
+      }
 	  },
 	  methods: {
 	    ...mapActions('siswa_ujian',[ 'startUjian', 'getPesertaUjian','getHasilUjian']),
       showError(err) {
         showSweetError(this, err)
+      },
+      getDiffMins(jadwal) {
+        const date = new Date()
+        const ye = date.getFullYear()
+        const mo = date.getMonth()
+        const da = date.getDate()
+        const mulai = jadwal.mulai
+        const splicer = mulai.split(":")
+        const h = parseInt(splicer[0])
+        const i = parseInt(splicer[1])
+        const s = parseInt(splicer[2])
+        const rest = new Date(ye,mo,da,h,i,s)
+
+        let today = new Date();
+        let diffMs = (rest - today);
+        let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
+
+        return diffMins;
       },
       async ujianStart(){
         try {
