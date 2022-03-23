6ab1ee1a60a5a2cf5afa86ea710aa5820a7ac1d0
shellrean <reanyusufali@gmail.com>
2022-03-23 09:00:39 +0700

    change zoomer eng

--- resources/js/pages/siswa/Ujian.vue
+++ resources/js/pages/siswa/Ujian.vue
@@ -189,7 +189,9 @@
                     <svg class="fill-current hidden w-4 h-4 text-green-500 pointer-events-none" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>
                   </div>
                 </div>
-                <div v-html="jawab.text_jawaban"></div>
+                <!-- <div v-html="jawab.text_jawaban"></div> -->
+                <RenderString
+                  :string="changeToZoomer(jawab.text_jawaban)" />
               </div>
             </template>
             <div
